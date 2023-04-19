a425ad9a5fef64069f0c5b3aba35993f7dc059e5
metacoding <getinthere@naver.com>
2023-04-19 21:17:29 +0900

    initial commit

---
+++ src/main/java/shop/mtcoding/thereblog/ThereblogApplication.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.thereblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThereblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThereblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/mtcoding/thereblog/ThereblogApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.thereblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThereblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
